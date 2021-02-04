91e0d3bd861fd2ca6a8d3c3e4f160a00fd8e1fa7
b6320500697 <78525157+NewAlisa@users.noreply.github.com>
2021-02-04 16:16:12 +0700

    3-11

--- 6320500697_3.c
+++ 6320500697_3.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main()
 {
-    int N,j,k;
+    long int N,j,k;
     scanf ("%d",&N);
-    j= N%3;
-    k= N%11;
+    j=N%3;
+    k=N%11;
     printf ("%d %d",j,k);
     return 0;
 }
