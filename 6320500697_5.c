cbfaaef969929a145e42fa3457bd1f25407a574d
b6320500697 <78525157+NewAlisa@users.noreply.github.com>
2021-02-04 16:54:50 +0700

    Max N

---
+++ 6320500697_5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,max,j;
+    scanf ("%d",&n);
+    int m[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&m[i]);
+        if(m[i]>max)
+        {
+
+			max=m[i];
+			j=i+1;
+
+		}
+    }
+    printf ("%d %d",j,max);
+
+}
